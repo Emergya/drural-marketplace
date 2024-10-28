70e8fa581961d15a79c8c5b433cbcf5b86b71b92
JMCBProxya <jmcobena@proxya.com>
2024-10-28 13:13:37 +0100

    Added bew function at sdk to sing in with openId Added funcionality to storefront to register and sing in with Google.

---
+++ sdk/src/data/ApolloClientManager/types.ts
@@ -0,0 +1,47 @@
+import { ApolloError } from "apollo-client";
+
+export enum PendingSaveItems {
+  UPDATE_CART = "updateCart",
+  BILLING_ADDRESS = "billingAddress",
+  SHIPPING_ADDRESS = "shippingAddress",
+  SHIPPING_AS_BILLING_ADDRESS = "shippingAsBillingAddress",
+}
+
+export interface ApolloErrorWithUserInput extends ApolloError {
+  extraInfo: {
+    userInputErrors?: any[];
+  };
+}
+
+export interface IApolloClientManagerResponse<T> {
+  data?: T;
+  error?: ApolloErrorWithUserInput;
+}
+
+export interface VerifySignInTokenInput {
+  token: string;
+}
+
+export interface RefreshSignInTokenInput {
+  csrfToken?: string | null;
+  refreshToken?: string;
+}
+
+export interface CreatePaymentInput {
+  amount: number;
+  checkoutId: string;
+  gateway: string;
+  token?: string;
+  returnUrl?: string;
+}
+
+export interface CompleteCheckoutInput {
+  checkoutId: string;
+  paymentData?: object;
+  redirectUrl?: string;
+  storeSource?: boolean;
+}
+
+export interface RemoveCheckoutInput {
+  checkoutId: string;
+}
